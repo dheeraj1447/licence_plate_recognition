75cb320b062c57f7c950108d46794e13761ddc12
Dheeraj Varanasi <70004355+dheeraj1447@users.noreply.github.com>
2023-11-19 20:16:31 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,63 +1,39 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog
+from tkinter import messagebox
 from PIL import Image, ImageTk
-
+from datetime import datetime
 from main import process_image
 
-
-class ImageUploaderApp:
-    def __init__(self, root):
+class ImageTimestampApp:
+    def _init_(self, root):
         self.root = root
         self.root.title("Smart Parking System")
         self.image1 = None
         self.image2 = None
         self.file_path1 = None
         self.file_path2 = None
+        self.timestamp1 = None
+        self.timestamp2 = None
 
         self.create_widgets()
         self.center_window()
 
     def create_widgets(self):
         # Title label
-        self.title_label = tk.Label(self.root, text="Smart Parking System", font=("Arial", 20))
-        self.title_label.pack(pady=20)
+        self.title_label = tk.Label(self.root, text="Image Timestamps", font=("Arial", 16))
+        self.title_label.pack(pady=10)
 
-        self.frame_left = tk.Frame(self.root, width=200, height=200)
-        self.frame_left.pack_propagate(False)
-        self.frame_left.pack(side=tk.LEFT, padx=10, pady=10)
-
-        self.frame_right = tk.Frame(self.root, width=200, height=200)
-        self.frame_right.pack_propagate(False)
-        self.frame_right.pack(side=tk.RIGHT, padx=10, pady=10)
-
-        self.upload_button1 = tk.Button(self.frame_left, text="Entry Gate", command=self.upload_image1)
+        # Buttons for uploading images
+        self.upload_button1 = tk.Button(self.root, text="Entry Gate", command=self.upload_image1)
         self.upload_button1.pack(pady=(10, 0), padx=10)
 
-        self.upload_button2 = tk.Button(self.frame_right, text="Exit Gate", command=self.upload_image2)
+        self.upload_button2 = tk.Button(self.root, text="Exit Gate", command=self.upload_image2)
         self.upload_button2.pack(pady=(10, 0), padx=10)
 
-        self.image_label1 = tk.Label(self.root)
-        self.image_label1.pack(fill=tk.BOTH, expand=True)
-
-        self.image_label2 = tk.Label(self.root)
-        self.image_label2.pack(fill=tk.BOTH, expand=True)
-
-        self.frame_bottom = tk.Frame(self.root)
-        self.frame_bottom.pack(pady=10)
-
-        self.match_result_button = tk.Button(self.frame_bottom, text="Match Result", command=self.show_alert)
-        self.match_result_button.pack()
-        self.match_result_button['state'] = 'disabled'
-
-    def check_images_uploaded(self):
-        if self.image1 and self.image2:
-            self.match_result_button['state'] = 'normal'  # Enable the button
-        else:
-            self.match_result_button['state'] = 'disabled'  # Keep the button disabled
-
     def center_window(self):
-        window_width = 800
-        window_height = 600
+        window_width = 1200
+        window_height = 800
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
         x_coordinate = int((screen_width / 2) - (window_width / 2))
@@ -68,29 +44,39 @@ class ImageUploaderApp:
         self.file_path1 = filedialog.askopenfilename()
         if self.file_path1:
             self.image1 = Image.open(self.file_path1)
-            self.show_image_in_label(self.image1, self.image_label1)
-            self.check_images_uploaded()
+            self.timestamp1 = datetime.now()
+            self.display_image_with_timestamp(self.image1, self.timestamp1.strftime("%Y-%m-%d %H:%M:%S"))
 
     def upload_image2(self):
         self.file_path2 = filedialog.askopenfilename()
         if self.file_path2:
             self.image2 = Image.open(self.file_path2)
-            self.show_image_in_label(self.image2, self.image_label2)
-            self.check_images_uploaded()
+            self.timestamp2 = datetime.now()
+            self.display_image_with_timestamp(self.image2, self.timestamp2.strftime("%Y-%m-%d %H:%M:%S"))
+            self.show_alert()
 
-    def show_image_in_label(self, img, label_widget):
+    def display_image_with_timestamp(self, img, timestamp):
         img = img.resize((200, 200))
         img = ImageTk.PhotoImage(img)
-        label_widget.config(image=img)
-        label_widget.image = img
+
+        # Display image
+        image_label = tk.Label(self.root, image=img)
+        image_label.image = img
+        image_label.pack(pady=10)
+
+        # Display timestamp
+        timestamp_label = tk.Label(self.root, text=f"Timestamp: {timestamp}", font=("Arial", 12))
+        timestamp_label.pack(pady=5)
 
     def show_alert(self):
         recognised_text_1 = process_image(self.file_path1)
         recognised_text_2 = process_image(self.file_path2)
         if recognised_text_1 is not None and recognised_text_2 is not None:
-            if recognised_text_1.__eq__(recognised_text_2):
+            if recognised_text_1._eq_(recognised_text_2):
+                difference = self.timestamp2 - self.timestamp1
+                total_seconds = difference.total_seconds()
                 messagebox.showinfo("SUCCESS",
-                                    "The recognised licence plate number is - " + recognised_text_1)
+                                    "The recognised licence plate number is - " + recognised_text_1 + ". Proceed to pay $" + str(total_seconds * 10))
             else:
                 messagebox.showerror("ERROR",
                                      "The recognised licence plate numbers are - " + recognised_text_1 + " and "
@@ -99,8 +85,7 @@ class ImageUploaderApp:
             messagebox.showerror("ERROR",
                                  "Unable to recognize the characters on licence plates!")
 
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     root = tk.Tk()
-    app = ImageUploaderApp(root)
+    app = ImageTimestampApp(root)
     root.mainloop()
